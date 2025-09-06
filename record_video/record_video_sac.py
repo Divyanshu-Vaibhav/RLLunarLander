2423ecd30de6c1934764bc1c8e8276fe8776e176
Divyanshu Vaibhav <divyanshuvaibhav050603@gmail.com>
2025-09-06 13:21:36 +0530

    Enabled recording of few episodes using the best model of each policy

---
+++ record_video/record_video_sac.py
@@ -0,0 +1,17 @@
+import gymnasium as gym
+from gymnasium.wrappers import RecordVideo
+from stable_baselines3 import SAC, PPO
+
+def record_policy(model_path, env_id, out_dir, episodes=3, deterministic=True):
+    env = gym.make(env_id, render_mode="rgb_array") 
+    env = RecordVideo(env, video_folder=out_dir, episode_trigger=lambda eid: True, name_prefix="eval")
+    model = (SAC if "sac" in model_path.lower() else PPO).load(model_path)
+    for ep in range(episodes):
+        obs, info = env.reset(seed=100+ep)
+        done = False; trunc = False
+        while not (done or trunc):
+            action, _ = model.predict(obs, deterministic=deterministic)
+            obs, reward, done, trunc, info = env.step(action)
+    env.close()
+
+record_policy("./models/sac_lander_cont_best/best_model", "LunarLanderContinuous-v3", "./videos/sac_cont")
